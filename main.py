dfad48687a86d58587fff325f4bfe5fcada225b8
John Cleaver <cleaver.john.k@gmail.com>
2012-07-26 20:46:14 -0400

    Added a basic main file.

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html', page_title="Home", org="CWRU", root_url="/")
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
