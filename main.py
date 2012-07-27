0ee0ce132f17982ac11d8530e97d1af72da4139e
John Cleaver <cleaver.john.k@gmail.com>
2012-07-26 22:00:19 -0400

    Added a route and a mock for the champion list page.

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html', page_title="Home", org="CWRU", root_url="/")
 
+@app.route('/champions')
+def champions():
+	champions = [{'name': 'Amumu'}, 
+				 {'name': 'Akali'}, 
+				 {'name': 'Alistar'}, 
+				 {'name': 'Ahri'}, 
+				 {'name': 'Anivia'}, 
+				 {'name': 'Annie'}, 
+				 {'name': 'Ashe'}, 
+				 {'name': 'Blitzcrank'}, 
+				 {'name': 'Brand'}, 
+				 {'name': 'Caitlyn'}, 
+				 {'name': 'Cassiopeia'}, 
+				 {'name': 'Galio'}, 
+				 {'name': 'Gangplank'}, 
+				 {'name': 'Jayce'}]
+
+	return render_template('champion_list.html', page_title="Champions", org="CWRU", root_url="../", champions=champions)
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
