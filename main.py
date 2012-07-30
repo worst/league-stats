37faee2769064a46ed33d411a942a24201e18f6c
John Cleaver <cleaver.john.k@gmail.com>
2012-07-30 13:53:31 -0400

    Updated main with mocking information for the new template.

--- main.py
+++ main.py
@@ -26,9 +26,26 @@ def champions():
 
 @app.route('/champions/<champ>')
 def champ_info(champ):
-	champion = {'name': champ, 'portrait': "http://placehold.it/320x240", "subtitle": "The Damage Dealer"}
+	champion = {'name': champ, 'portrait': "http://placehold.it/320x240", "subtitle": "The Defender of Tomorrow"}
 
 	return render_template('champion.html', page_title=champ, org="CWRU", root_url="../../", champion=champion, summoner_wins='', summoner_percent="")
+
+@app.route('/league')
+def league():
+	most_wins = {'summoner': 'Kihashi',
+				 'wins': 500}
+	percentage = {'summoner': 'PuddinPop',
+				  'wins': 60}
+	games = {'summoner': "ONLY MID THX",
+			 'number': 10000}
+	most_popular = {'champion': 'Amumu',
+					'thumbnail': 'AmumuSquare.png',
+					'games': 100}
+	winningest = {'champion': 'Singed',
+					'thumbnail': 'SingedSquare.png',
+					'percent': 80}
+
+	return render_template('league.html', page_title="League Statistics", org="CWRU", root_url="../", most_wins=most_wins, percentage=percentage, games=games, most_popular=most_popular, winningest=winningest)
 
 if __name__ == '__main__':
 	app.debug = True
