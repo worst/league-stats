b44a5630d60730ddb075c13af88090da39580aaf
John Cleaver <cleaver.john.k@gmail.com>
2012-08-14 15:17:07 -0400

    Converted the model file to PEP8.

--- models/models.py
+++ models/models.py
@@ -3,12 +3,13 @@ from elixir import *
 metadata.bind = "sqlite:///database.sqlite"
 metadata.bind.echo = True
 
-class Champion(Entity):
-	name = Field(Unicode(20))
-	title = Field(Unicode(30))
-	thumbnail = Field(Text())
-	splash_art = Field(Text())
-	rel_date = Field(Date())
 
-	def __repr__(self):
-		return '<Champion %s, %s>' % (self.name, self.title)
+class Champion(Entity):
+    name = Field(Unicode(20))
+    title = Field(Unicode(30))
+    thumbnail = Field(Text())
+    splash_art = Field(Text())
+    rel_date = Field(Date())
+
+    def __repr__(self):
+        return '<Champion %s, %s>' % (self.name, self.title)
