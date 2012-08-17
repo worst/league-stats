ed5854c7f5903c81d19be1a40a48abff11d5413e
John Cleaver <cleaver.john.k@gmail.com>
2012-08-17 13:31:41 -0400

    Added new entries to the model.

--- models/models.py
+++ models/models.py
@@ -13,3 +13,33 @@ class Champion(Entity):
 
     def __repr__(self):
         return '<Champion %s, %s>' % (self.name, self.title)
+
+
+class Summoner(Entity):
+    summoner_name = Field(Unicode(30))
+    fname = Field(Unicode(15))
+    lname = Field(Unicode(30))
+    year = Field(Integer)
+    teams = ManyToMany('Team')
+
+    def __repr__(self):
+        return '<%s %s (%s)>' %(self.fname, self.lname, self.summoner_name)
+
+
+class Side(Entity):
+    color = Field(Integer)
+    teams = OneToMany('Team')
+
+
+class Team(Entity):
+    side = ManyToOne('Side')
+    summoners = ManyToMany('Summoner')
+    champions = ManyToMany('Champion')
+    game = ManyToOne('Game')
+
+
+class Game(Entity):
+    teams = OneToMany('Team')
+    winner = OneToOne('Game')
+    date = Field(Date)
+    length = Field(Integer)
